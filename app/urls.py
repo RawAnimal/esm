5562a23eec10c03e7d7c50b5759c782f1f602169
rawanimal <skjsweeney@gmail.com>
2021-09-11 13:02:26 +0000

    Updated paths to project root

--- app/urls.py
+++ app/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/user/", include("user.urls")),
-    path("project/", include("project.api.urls")),
+    path("user/", include("user.urls")),
+    path("", include("project.urls")),
 ]
