ac63e871d843ed80e3070cbe149eb5304c4cd6d9
rawanimal <skjsweeney@gmail.com>
2021-09-11 13:04:36 +0000

    Added model serializers

---
+++ project/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from .models import Client, Project, ProjectClients
+
+
+class ProjectClientsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectClients
+        fields = "__all__"
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    clients = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Project
+        fields = ["temp_name", "clients"]
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+    projects = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Client
+        fields = ["full_name", "first_name", "last_name", "projects"]
+
+    def get_full_name(self, object):
+        return f"{object.first_name} {object.last_name}"
