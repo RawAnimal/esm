f379cff69cb33b3d54f53da1bec95d71c1891e6e
rawanimal <skjsweeney@gmail.com>
2021-09-11 13:03:47 +0000

    reduced client and project models

--- project/models.py
+++ project/models.py
@@ -1,9 +1,45 @@
 from django.db import models
 
+# from django.utils import timezone
+
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    projects = models.ManyToManyField("Project", through="ProjectClients")
+
+    class Meta:
+        db_table = "clients"
+        verbose_name = "Client"
+        verbose_name_plural = "Clients"
+
+    def __str__(self) -> str:
+        return self.first_name + " " + self.last_name
+
 
 class Project(models.Model):
-    name = models.CharField(max_length=100)
-    active = models.BooleanField(default=True)
+    temp_name = models.CharField(max_length=30)
+    clients = models.ManyToManyField("Client", through="ProjectClients")
 
-    def __str__(self):
-        return self.name
+    class Meta:
+        db_table = "projects"
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+
+    def __str__(self) -> str:
+        return self.temp_name
+
+
+class ProjectClients(models.Model):
+    client = models.ForeignKey(
+        "Client", on_delete=models.PROTECT, related_name="project_client_list"
+    )
+    project = models.ForeignKey(
+        "Project", on_delete=models.PROTECT, related_name="client_project_list"
+    )
+    effective_date = models.DateField()
+
+    class Meta:
+        db_table = "project_clients"
+        verbose_name = "Project Clients"
+        verbose_name_plural = "Project Clients"
