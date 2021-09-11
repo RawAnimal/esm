03d3f9d065b9985c41150b33a518bd1a3da35570
rawanimal <skjsweeney@gmail.com>
2021-09-11 13:07:54 +0000

    Updated files

--- project/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from project.api.views import project_list, project_details
-
-urlpatterns = [
-    path("", project_list, name="project-list"),
-    path("<int:pk>", project_details, name="project-details"),
-]
